ea4fbfe75ac9ce4040939da781129572a154f600
Kim Jin-Seong <acmiheee@gmail.com>
2022-11-16 22:18:01 +0900

    feat: add vuetify

--- index/src/App.vue
+++ index/src/App.vue
@@ -1,41 +1,23 @@
-<!-- 
-      - Vue 애플리케이션의 최상위 컴포넌트
--->
-/* eslint-disable */
-
 <template>
-  <Header></Header>
-  <router-view></router-view>
-  <Footer></Footer>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Header from './components/common/Header.vue';
-import Footer from './components/common/Footer.vue';
-import Main from './components/common/Main.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    Header,
-    Footer,
-    Main
-  }
-}
+    HelloWorld,
+  },
 
+  data: () => ({
+    //
+  }),
+}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
-}
-body > div{
-  min-height: 300px;
-  background-color: #fdfdfd;
-}
-</style>
