bd125164d5e6de29a6004eb5107ed7580d929282
KimJinSeong-Git <acmiheee@gmail.com>
2022-11-22 17:39:49 +0900

    feat: banner Text

--- index/src/components/common/Main.vue
+++ index/src/components/common/Main.vue
@@ -1,7 +1,23 @@
 <template>
+  <head>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  </head>
+
   <v-img
     src="../../assets/banner.jpg"
-  ></v-img>
+  >
+    <div class="banner-text">
+      <p class="main-text">우리 학교</p>
+      <p class="main-text">찰떡궁합 내 친구</p>
+      <p class="main-text">어디있니..?</p>
+      <p class="side-text">MBTI를 통해 친구를 찾아보세요.</p>
+      <v-btn
+        color="secondary"
+        elevation="2"
+        x-large
+      ><p class="side-text">GO!</p></v-btn>
+    </div>
+  </v-img>
 </template>
 
 <script>
@@ -11,4 +27,22 @@ export default{
 </script>
 
 <style>
+div.banner-text{
+  margin-left: 10%;
+  margin-top: 5%;
+  color: white;
+  text-shadow: 2px 2px 2px #999999;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+p.main-text{
+  font-size: 32px;
+  font-weight: bold;
+}
+p.side-text{
+  padding: 3px 3px;
+  font-size: 12px;
+}
+v-btn.test{
+  color: white
+}
 </style>
