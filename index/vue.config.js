142ec294b4132cb83c3583ec311addfb46773a59
Kim Jin-Seong <acmiheee@gmail.com>
2022-12-03 17:26:23 +0900

    feat: build

--- index/vue.config.js
+++ index/vue.config.js
@@ -2,11 +2,22 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: "../../mYting_API/public",
+  outputDir: '../../mYting_API/backend/public',
 
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://127.0.0.1:3000/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
+  },
 })
