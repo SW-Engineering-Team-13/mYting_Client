b1a6ef8f0e5f58f914bbc8fd097857e8b855959f
임정호 <msk4654@yonsei.ac.kr>
2022-12-05 13:03:45 +0900

    스터디 편집, 장소예약

--- index/src/views/Study/EditStudyProfile.vue
+++ index/src/views/Study/EditStudyProfile.vue
@@ -1,14 +1,7 @@
 <template>
   <v-container>
-    <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-    >
-    <v-col
-        cols="12"
-        md="10"
-      >
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-col cols="12" md="10">
         <v-textarea
           v-model="name"
           :counter="30"
@@ -17,22 +10,16 @@
         ></v-textarea>
       </v-col>
 
-      <v-col
-          cols="12"
-          md="10"
-        >
-          <v-textarea
-            v-model="introduction"
-            :counter="200"
-            label="스터디 소개"
-            rows="4"
-          ></v-textarea>
-        </v-col>
+      <v-col cols="12" md="10">
+        <v-textarea
+          v-model="introduction"
+          :counter="200"
+          label="스터디 소개"
+          rows="4"
+        ></v-textarea>
+      </v-col>
 
-    <v-col
-        cols="12"
-        md="10"
-      >
+      <v-col cols="12" md="10">
         <v-textarea
           v-model="rules"
           :counter="400"
@@ -44,32 +31,81 @@
       <v-select
         v-model="select"
         :items="memcount"
-        :rules="[v => !!v || '정원제한을 설정해주세요!']"
+        :rules="[(v) => !!v || '정원제한을 설정해주세요!']"
         label="스터디 정원"
         required
-        ></v-select>
+      ></v-select>
 
-  <v-app>
-    <div>
-      <v-btn
-      value="left"
-      color="warning"
-      elevation="2"
-      small
-      @click="editDone"
-      >수정완료
-      </v-btn>
+      <v-app>
+        <div>
+          <v-btn
+            value="left"
+            color="warning"
+            elevation="2"
+            small
+            @click="editDone"
+            >수정완료
+          </v-btn>
 
-      <v-btn
-      value="right"
-      color="error"
-      elevation="2"
-      small
-      @click="exit"
-      >취소
-      </v-btn>
-    </div>
-  </v-app>
-  </v-form>
-</v-container>
+          <v-btn value="right" color="error" elevation="2" small @click="exit"
+            >취소
+          </v-btn>
+        </div>
+      </v-app>
+    </v-form>
+  </v-container>
+
+  <div v-show="is_show">
+    <p>스터디가 수정되었습니다.</p>
+
+    <button @click="handle_toggle" type="button">확인</button>
+  </div>
 </template>
+
+<script>
+import Nav from "@/views/Study/StudyNav.vue";
+
+export default {
+  data() {
+    return {
+      memcount: ["2", "3", "4", "5", "6"],
+      name: "",
+      nameRules: [
+        (v) => !!v || "스터디 이름을 적으세요",
+        (v) => (v && v.length <= 30) || "이름은 30자 이내여야 합니다.",
+      ],
+      introduction: "",
+      introductionRules: [
+        (v) => !!v || "스터디 소개를 적으세요",
+        (v) => (v && v.length <= 200) || "소개는 200자 이내여야 합니다.",
+      ],
+      rules: "",
+      rulesRules: [
+        (v) => !!v || "스터디 규칙을 적으세요",
+        (v) => (v && v.length <= 400) || "규칙은 400자 이내여야 합니다.",
+      ],
+      select: 2,
+      is_show: false,
+    };
+  },
+  components: {
+    Nav,
+  },
+  methods: {
+    MakeGroup() {
+      this.$http
+        .post("/api/editstudy", {
+          uid: uid,
+          studyname: this.name,
+        })
+        .then((res) => {
+          this.handle_toggle();
+          window.open("/study", "_blank");
+        });
+    },
+    handle_toggle() {
+      this.is_show = !this.is_show;
+    },
+  },
+};
+</script>
--- index/src/views/Study/PlaceReservation.vue
+++ index/src/views/Study/PlaceReservation.vue
@@ -1,33 +1,20 @@
 <template>
   <v-container>
-    <v-row
-      justify="center">
-      <v-col
-        cols="6">
+    <v-row justify="center">
+      <v-col cols="6">
         <v-img src="@/assets/PlaceReservation.png" />
       </v-col>
     </v-row>
-      <v-row
-        justify="center">
-        <v-chip 
-          class="ma-2"
-          href="https://space.yonsei.ac.kr/"
-        >
-          강의동 세미나실
-        </v-chip>
-        <v-chip 
-          class="ma-2"
-          href="https://wlib.yonsei.ac.kr/relation/seat"
-        >
-          중앙도서관 세미나실
-        </v-chip>
-      </v-row>
-    <v-row
-      justify="center"
-    >
-      <v-col
-        cols="6"
-      >
+    <v-row justify="center">
+      <v-chip class="ma-2" href="https://space.yonsei.ac.kr/">
+        강의동 세미나실
+      </v-chip>
+      <v-chip class="ma-2" href="https://wlib.yonsei.ac.kr/relation/seat">
+        중앙도서관 세미나실
+      </v-chip>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="6">
         <v-textarea
           v-model="place"
           :counter="30"
@@ -36,28 +23,57 @@
         />
       </v-col>
     </v-row>
-    <v-row
-      justify="center"
-    >
+    <v-row justify="center">
       <v-btn
         color="primary"
         elevation="2"
         small
         @click="reservationDone"
         to="/study"
-      >대관완료
+        >대관완료
       </v-btn>
     </v-row>
   </v-container>
+
+  <div v-show="is_show">
+    <p>스터디 장소가 등록되었습니다.</p>
+
+    <button @click="handle_toggle" type="button">확인</button>
+  </div>
+
   <Nav />
 </template>
 
 <script>
-import Nav from '@/views/Study/StudyNav.vue';
+import Nav from "@/views/Study/StudyNav.vue";
 
 export default {
-  components: {
-    Nav
+  data() {
+    return {
+      place: "",
+      is_show: false,
+    };
   },
-}
+
+  components: {
+    Nav,
+  },
+  methods: {
+    reservationDone() {
+      this.$http
+        .post("/api/placestudy", {
+          place: place,
+          studyid: studyid,
+        })
+        .then((res) => {
+          this.handle_toggle();
+          window.open("/study", "_blank");
+        });
+    },
+
+    handle_toggle() {
+      this.is_show = !this.is_show;
+    },
+  },
+};
 </script>
